f81c081fd056677c99feb63ff82a310382d6eb0f
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2013-04-20 14:45:21 -0400

    Overloaded firstIndexOf to accept iterables

--- src/edu/pitt/isp/sverchkov/arrays/ArrayTools.java
+++ src/edu/pitt/isp/sverchkov/arrays/ArrayTools.java
@@ -9,6 +9,7 @@ package edu.pitt.isp.sverchkov.arrays;
  * @author YUS24
  */
 public class ArrayTools {
+    
     public static boolean contains( Object target, Object array[] ){
         return firstIndexOf( target, array ) != -1;
     }
@@ -22,6 +23,23 @@ public class ArrayTools {
                 for( int i=0; i<array.length; i++ )
                     if( target.equals( array[i] ) ) return i;
             }
+        return -1;
+    }
+    
+    public static int firstIndexOf( Object target, Iterable array ){
+        if( array != null ){
+            int i=0;
+            if( target == null )
+                for( Object o : array ){
+                    if( o == null ) return i;
+                    ++i;
+                }
+            else
+                for( Object o : array ){
+                    if( target.equals(o) ) return i;
+                    ++i;
+                }
+        }
         return -1;
     }
 }
