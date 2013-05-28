4d64eda63720ae621a0ac440e2bd1fdd49086fa4
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2013-05-28 17:31:41 -0400

    Renamed arrays package to collections package

--- src/edu/pitt/isp/sverchkov/collections/ArrayTools.java
+++ src/edu/pitt/isp/sverchkov/collections/ArrayTools.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.pitt.isp.sverchkov.arrays;
+package edu.pitt.isp.sverchkov.collections;
 
 /**
  *
