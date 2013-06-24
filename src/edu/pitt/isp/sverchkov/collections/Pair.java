b97a3e22f6cd36f9a16e49398d1a7fd8f6d21819
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2013-06-24 13:21:54 -0400

    Added a pair class

---
+++ src/edu/pitt/isp/sverchkov/collections/Pair.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pitt.isp.sverchkov.collections;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author YUS24
+ */
+public class Pair<First,Second> {
+    
+    public final First first;
+    public final Second second;
+    
+    public Pair( First f, Second s ){
+        first = f;
+        second = s;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pair<First, Second> other = (Pair<First, Second>) obj;
+        if (!Objects.equals(this.first, other.first)) {
+            return false;
+        }
+        if (!Objects.equals(this.second, other.second)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.first);
+        hash = 53 * hash + Objects.hashCode(this.second);
+        return hash;
+    }
+    
+}
