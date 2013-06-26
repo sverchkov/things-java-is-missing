530f1bebf2522367560ece54a6b4c9958fbe7f32
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2013-06-26 14:04:50 -0400

    Added applyMap

--- src/edu/pitt/isp/sverchkov/collections/CollectionTools.java
+++ src/edu/pitt/isp/sverchkov/collections/CollectionTools.java
@@ -4,6 +4,7 @@
  */
 package edu.pitt.isp.sverchkov.collections;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +17,18 @@ public class CollectionTools {
     public static <K,V> Map<K,V> mapUnion( Map<K,V> m1, Map<K,V> m2 ){
         Map<K,V> result = new HashMap<>(m1);
         result.putAll(m2);
+        return result;
+    }
+    
+    public static <Key,Value> ArrayList<Value> applyMap( Map<? extends Key, ? extends Value> map, Key[] keys ){
+        ArrayList<Value> result = new ArrayList<>();
+        for( Key k : keys ) result.add( map.get(k) );
+        return result;
+    }
+
+    public static <Key,Value> ArrayList<Value> applyMap( Map<? extends Key,? extends Value> map, Iterable<? extends Key> keys ){
+        ArrayList<Value> result = new ArrayList<>();
+        for( Key k : keys ) result.add( map.get(k) );
         return result;
     }
 }
