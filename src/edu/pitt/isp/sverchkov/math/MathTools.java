c4b82988410904931f6c573646a1c661a35cfe11
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2013-07-23 13:22:15 -0400

    Added memoized log( n! )

--- src/edu/pitt/isp/sverchkov/math/MathTools.java
+++ src/edu/pitt/isp/sverchkov/math/MathTools.java
@@ -4,11 +4,17 @@
  */
 package edu.pitt.isp.sverchkov.math;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author YUS24
  */
 public class MathTools {
+    
+    private static final List<Double> logFactorials = new ArrayList<>();
     
     //TODO public static int sum( int[] array ){        
     //}
@@ -23,5 +29,21 @@ public class MathTools {
         double sum = 0;
         for( Number num : nums ) sum += num.doubleValue();
         return sum;
-    }  
+    }
+    
+    /**
+     * Memoized ln( n! )
+     * @param n
+     * @return ln( n! )
+     */
+    public static double logFactorial( int n ){
+        int last = logFactorials.size()-1;
+        if( n > last ){
+            double lf = logFactorials.get( last );
+            while( last < n )
+                logFactorials.add( lf += Math.log( ++last ) );
+            return lf;
+        }
+        return logFactorials.get(n);
+    }
 }
