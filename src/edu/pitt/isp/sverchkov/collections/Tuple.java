0579e61220d0df8319b7eebf6dd6548c27201fde
YUS24 <YUS24@DBMI-DT-101.univ.pitt.edu>
2013-08-01 15:50:32 -0400

    Added comparator by first tuple entry

--- src/edu/pitt/isp/sverchkov/collections/Tuple.java
+++ src/edu/pitt/isp/sverchkov/collections/Tuple.java
@@ -4,6 +4,8 @@
  */
 package edu.pitt.isp.sverchkov.collections;
 
+import java.util.Comparator;
+
 /**
  *
  * @author YUS24
@@ -11,4 +13,11 @@ package edu.pitt.isp.sverchkov.collections;
 public interface Tuple<First, Rest extends Tuple<?,?>> {
     First getFirst();
     Rest getRest();
+    
+    public static class CompareByFirst<Thing extends Comparable<Thing>> implements Comparator<Tuple<Thing,?>> {
+        @Override
+        public int compare(Tuple<Thing, ?> o1, Tuple<Thing, ?> o2) {
+            return o1.getFirst().compareTo(o2.getFirst());
+        }
+    }
 }
